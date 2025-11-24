709642b8b7f58a4a4633b6df759282260022789b
kayllaneAs <kayllanearauho838@gmail.com>
2025-11-24 10:14:17 -0300

    Feat: Atualização no main

--- js/main.js
+++ js/main.js
@@ -171,8 +171,8 @@ function initializePageComponents(pageName) {
             if (typeof MenuPage !== 'undefined') MenuPage.initialize();
             break;
 
-        case 'guiaCuidados':
-            if (typeof GuiaCuidadosPage !== 'undefined') GuiaCuidadosPage.initialize();
+        case 'cuidados':
+            if (typeof CuidadosPage !== 'undefined') GuiaCuidadosPage.initialize();
             break;
         case 'feedbacks':
             if (typeof FeedbacksPage !== 'undefined') FeedbacksPage.initialize();
